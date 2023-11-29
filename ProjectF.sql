58ff4a4db0c47251432ac65259b47299739ee8dd
Leo <122913304+Leo-Hoffman@users.noreply.github.com>
2023-11-29 13:08:21 -0600

    Update ProjectF.sql

--- ProjectF.sql
+++ ProjectF.sql
@@ -41,10 +41,10 @@ CREATE TABLE shows (
 
 CREATE TABLE episode (
 	episode_id INT PRIMARY KEY AUTO_INCREMENT,
-   	 episode_title VARCHAR(50) NOT NULL,
+   	episode_title VARCHAR(50) NOT NULL,
 	show_id INT NOT NULL,
     	episode_number INT NOT NULL,
-   	 season_number INT NOT NULL,
+   	season_number INT NOT NULL,
    	 CONSTRAINT
 		FOREIGN KEY (show_id)
 		REFERENCES shows(show_id), 
@@ -55,13 +55,14 @@ CREATE TABLE episode (
 --show_id = 1
 INSERT INTO shows (show_name, start_year, end_year, number_of_seasons)
 VALUES 
-	('The Sopranos', 1999, 2007, 1);
-	('Breaking Bad', 2008, 2013, 1);
+	('The Sopranos', 1999, 2007, 6),
+	('Wednesday', 2022, NULL, 1),
+	('Breaking Bad', 2008, 2013, 5),
 	('Squid Game,', 2021, 2021, 1), 
 	('The Bear', 2021, 2021, 1),
 	('The Shield', 2002, 2008, 1), 
 	('Fargo', 2014, NULL, 1),
-	('Game of Thrones', 2011, 2019, 1), 
+	('Game of Thrones', 2011, 2019, 9), 
 	('BattleStar Galactica', 1978, 1979, 1),
 	('The Tick', 2001, 2002, 1), 
 	('Firefly', 2002, 2003, 1),
@@ -70,24 +71,23 @@ VALUES
 	('Freaks and Geeks', 1999, 2000, 1), 
 	('Police Squad', 1982, 1982, 1),
 	('Undeclared', 2001, 2002, 1), 
-	('Sherlock', 2010, 2017, 1),
+	('Sherlock', 2010, 2017, 4),
 	('My So Called Life', 1994, 1995, 1), 
 	('Clone High', 2002, 2003, 1),
 	('Flashforward', 2009, 2010, 1), 
 	('Total Drama', 2007, 2008, 1),
 	('The Big Bang Theory', 2007, 2019, 1), 
 	('Regular Show', 2010, 2017, 1),
-	('Stranger Things', 2016, 2023, 1), 
-	('Better Call Saul', 2008, 2013, 1),
+	('Stranger Things', 2016, 2023, 4), 
+	('Better Call Saul', 2008, 2013, 6),
 	('Gravity Falls', 2015, 2016, 1), 
-	('Twin Peaks', 1989, 1991, 1);
+	('Twin Peaks', 1989, 1991, 3);
 	('Alice in Borderland', 2020, NULL, 2),
-	('Wednesday', 2022, NULL, 1),
 	('Insecure', 2016, 2021, 5),
 	('The Queen\`s Gambit', 2020, 2020, 1);
 	('Unorthodox', 2020, 2020, 1);
 	('The Fall of the House of Usher', 2023, 2023, 1);
-	('Gaslit', 2023, 2023, 1);
+	('Gaslit', 2023, 2023, 1),
 	('Dahmer - Monster: The Jeffrey Dahmer Story', 2023, NULL, 1);
 	('Snabba Cash', 2021, 2022, 2);
 	('Maid', 2021, 2021, 1);
@@ -95,6 +95,8 @@ VALUES
 	('The Twelve', 2019, 2023, 2);
 	('Superwog', 2018, 2021, 2);
 	('The Carrie Diaries', 2013, 2014, 2);
+
+-- Episodes for The Carrie Diaries, Season 1
 
 INSERT INTO episode (episode_title, show_id, episode_number, season_number)
 VALUES
@@ -133,7 +135,7 @@ VALUES
 	('Episode 7', 1, 7, 2),
 	('Episode 8', 1, 8, 2);
 
--- all episode for show id=2 
+-- all episode for show id=2, Wednesday
   
 INSERT INTO episode (episode_title, show_id, episode_number, season_number)
 VALUES ('Wednesday\'s Child is Full of Wow', 2, 1, 1), 
