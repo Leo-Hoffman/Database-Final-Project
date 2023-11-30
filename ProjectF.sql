4a356a767710dd685c2e8752b678ca251d17debe
mondiriariana <114170066+mondiriariana@users.noreply.github.com>
2023-11-30 12:12:24 -0600

    Update ProjectF.sql

--- ProjectF.sql
+++ ProjectF.sql
@@ -92,7 +92,7 @@ VALUES
 	('Snabba Cash', 2021, 2022, 2);
 	('Maid', 2021, 2021, 1);
 	('Boo, Bitch', 2022, 2022, 1);
-	('The Twelve', 2019, 2023, 2);
+	('The Twelve', 2019, NULL, 2);
 	('Superwog', 2018, 2021, 2);
 	('The Carrie Diaries', 2013, 2014, 2);
 
@@ -141,37 +141,93 @@ VALUES 		('Pilot', 39, 1, 1),
 		('The Magpie', 39, 3, 2),
 		('Something Fishy', 39, 4, 2),
 		('The Lump', 39, 5, 2),
-		('Jump Ya', 39, 6, 2),
+		('Jump Ya', 39, 6, 2);
 
--- Episodes for The Carrie Diaries, Season 1
-
+-- Episodes for show_id = 38 name='The Twelve'
 INSERT INTO episode (episode_title, show_id, episode_number, season_number)
 VALUES
-	('Episode 1', 40, 1, 1), 
-	('Episode 2', 40, 2, 1),
-	('Episode 3', 40, 3, 1),
-	('Episode 4', 40, 4, 1),
-	('Episode 5', 40, 5, 1),
-	('Episode 6', 40, 6, 1),
-	('Episode 7', 40, 7, 1),
-	('Episode 8', 40, 8, 1),
-	('Episode 1', 40, 9, 1), 
-	('Episode 2', 40, 10, 1),
-	('Episode 3', 40, 11, 1),
-	('Episode 4', 40, 12, 1),
-	('Episode 5', 40, 13, 1),
+	('Episode #1.1', 38, 1, 1), 
+	('Episode #1.2', 38, 2, 1),
+	('Episode #1.3', 38, 3, 1),
+	('Episode #1.4', 38, 4, 1),
+	('Episode #1.5', 38, 5, 1),
+	('Episode #1.6', 38, 6, 1),
+	('Episode #1.7', 38, 7, 1),
+	('Episode #1.8', 38, 8, 1),
 
- -- all episode for show_id = 1
+
+-- Episodes for show_id = 37 name='Boo bitch'
 INSERT INTO episode (episode_title, show_id, episode_number, season_number)
 VALUES
-	('Episode 1', 1, 1, 1), 
-	('Episode 2', 1, 2, 1),
-	('Episode 3', 1, 3, 1),
-	('Episode 4', 1, 4, 1),
-	('Episode 5', 1, 5, 1),
-	('Episode 6', 1, 6, 1),
-	('Episode 7', 1, 7, 1),
-	('Episode 8', 1, 8, 1),
+	('Life is a Bitch and Then You Die', 37, 1, 1), 
+	('Resting Bitch Face', 37, 2, 1),
+	('Payback is a Bitch', 37, 3, 1),
+	('Bitch Slapped', 37, 4, 1),
+	('Fake Bitch', 37, 5, 1),
+	('Who Dat Bitch?', 37, 6, 1),
+	('Bad Bitch', 37, 7, 1),
+	('Bitch, Bye', 37, 8, 1);
+
+ -- all episode for show_id = 36 name=Maid
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES
+	('Dollar Store', 36, 1, 1), 
+	('Ponies', 36, 2, 1),
+	('Sea Glass', 36, 3, 1),
+	('Cashmere', 36, 4, 1),
+	('Thief', 36, 5, 1),
+	('M', 36, 6, 1),
+	('String Cheese', 36, 7, 1),
+	('Bear Hunt', 36, 8, 1),
+	('Sky Blue', 36, 9, 1),
+	('Snaps', 36, 10, 1);
+
+
+ -- all episode for show_id = 35 name=Snabba Cash
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES
+	
+	('Shuno is 12 Percent', 35, 1, 1),
+	('Gonna Be 2 Rocks, Leya', 35, 2, 1),
+	('Friends and Family', 35, 3, 1),
+	('Was Never Bout the Cheese, Things Just Happened This Way', 35, 4, 1),
+	('If You Play with Fire, You're Gonna Get Burned', 35, 5, 1),
+	('Making Money Is Never Bad!', 35, 6, 1),
+
+        ('t Was Dag Hammarskjöld, Fam', 35, 1, 2), 
+	('Are They Chatting or Selling?', 35, 2, 2),
+	('We \' ll Try One with You', 35, 3, 2),
+	('Waste Them All', 35, 4, 2),
+	('Yeah, 100%. This Is the Last Last Time', 35, 5, 2),
+	('I Know Who You Are', 35, 6, 2);
+
+ -- all episode for show_id = 34  name= Dahmer - Monster: The Jeffrey Dahmer Story
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES
+
+	('Bad Meat', 34, 1, 1),
+	('Please Don \'t Go', 34, 2, 1),
+	('Doin \' A Dahmer', 34, 3, 1),
+	('The Good Boy Box', 34, 4, 1),
+	('Blood On Their Hands', 34, 5, 1),
+	('Silenced', 34, 6, 1),
+	('Cassandra', 34, 7, 1),
+	('Lionel', 34, 8, 1),
+	('The Bogeyman', 34, 9, 1),
+	('God of Forgiveness, God of Vengeance', 34, 10, 1);
+
+ -- all episode for show_id = 33  name=Gaslit
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES	
+
+	('Will', 33, 1, 1),
+	('California', 33, 2, 1),
+	('King George', 33, 3, 1),
+	('Malum in Se', 33, 4, 1),
+	('Honeymoon', 33, 5, 1),
+	('Tuffy', 33, 6, 1),
+	('Year of the Rat', 33, 7, 1),
+	('Final Days', 33, 8, 1),
 
         ('Episode 1', 1, 1, 2), 
 	('Episode 2', 1, 2, 2),
@@ -182,19 +238,42 @@ VALUES
 	('Episode 7', 1, 7, 2),
 	('Episode 8', 1, 8, 2);
 
--- all episode for show id=2, Wednesday
-  
+ -- all episode for show_id = 32  name=The Fall of the House of Usher
 INSERT INTO episode (episode_title, show_id, episode_number, season_number)
-VALUES ('Wednesday\'s Child is Full of Wow', 2, 1, 1), 
-	('Woe Is The Loneliest Number', 2, 2, 1),
-	('Friend or Woe', 2, 3, 1),
-	('Woe What a Night', 2, 4, 1),
-	('You Reap What You Woe', 2, 5, 1),
-	('Quid Prop Woe', 2, 6, 1),
-	('If You Don\'s Woe Me by Now', 2, 7, 1),
-	('A Merder of Woe', 2, 8, 1);
+VALUES	
 
--- all episode for show id=29, insecure
+	('A Midnight Dreary', 32, 1, 1),
+	('The Masque of the Red Death', 32, 2, 1),
+	('Murder in the Rue Morgue', 32, 3, 1),
+	('The Black Cat', 32, 4, 1),
+	('The Tell-Tale Heart', 32, 5, 1),
+	('Goldbug', 32, 6, 1),
+	('The Pit and the Pendulum', 32, 7, 1),
+	('The Raven', 32, 8, 1);
+
+ -- all episode for show_id = 31  name=Unorthodox
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES	
+	
+	('Part 1', 31, 1, 1),
+	('Part 2', 31, 2, 1),
+	('Part 3', 31, 3, 1),
+	('Part 4', 31, 4, 1);
+
+
+-- all episode for show_id = 30  name=The Queen\`s Gambit
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES	
+	
+	('Openings', 30, 1, 1),
+	('Exchanges', 30, 2, 1),
+	('Doubled Pawns', 30, 3, 1),
+	('Middle Game', 30, 4, 1),
+	('Fork', 30, 5, 1),
+	('Adjournment', 30, 6, 1),
+	('End Game', 30, 7, 1);
+
+-- all episode for show id=29, name=insecure
 INSERT INTO episode (episode_title, show_id, episode_number, season_number)
 VALUES 
             ('Insecure as Fuck', 7, 1, 1), 
@@ -228,12 +307,12 @@ VALUES
 		('Lowkey Distant', 7, 9, 4),
 		('Lowkey Thankful', 7, 3, 4),
 		('Lowkey Losin\' It', 7, 4, 4),
-		('Lokwey Movin\'n On', 7, 5, 4),
+		('Lokwey Movin On', 7, 5, 4),
 		('Lowkey Done', 7, 6, 4),
 		('Lowkey Trippin', 7, 7, 4),
 		('Lowkey Happy', 7, 8, 4),
 		('Lowkey Trying', 7, 9, 4),
-            ('Lowkey Lost', 7, 10, 4),
+                ('Lowkey Lost', 7, 10, 4),
 	
 		('Reunited, Okay?', 7, 2, 5),
 		('Growth, Okay?', 7, 3, 5),
@@ -244,6 +323,66 @@ VALUES
 		('Episode 8', 7, 8, 5),
 		('Episode 9', 7, 6, 5),
 		('Episode 10', 7, 7, 5);
+
+-- all episode for show id=28, name=Alice in Borderland
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES 
+            	('Episode 1', 28, 1, 1), 
+		('Episode 2', 28, 2, 1),
+		('Episode 3', 28, 3, 1),
+		('Episode 4', 28, 4, 1),
+		('Episode 5', 28, 5, 1),
+		('Episode 6', 28, 6, 1),
+		('Episode 7', 28, 7, 1),
+		('Episode 8', 28, 8, 1),
+	
+            	('Episode 1', 28, 1, 2), 
+		('Episode 2', 28, 2, 2),
+		('Episode 3', 28, 3, 2),
+		('Episode 4', 28, 4, 2),
+		('Episode 5', 28, 5, 2),
+		('Episode 6', 28, 6, 2),
+		('Episode 7', 28, 7, 2),
+		('Episode 8', 28, 8, 2);
+
+-- all episode for show id=26, name=Twin Peaks
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES 
+	
+-- all episode for show id=25, name=Gravity Falls
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES 
+-- all episode for show id=24, name=Better Call Saul
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES 
+	
+-- all episode for show id=23 name=Stranger Things
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES 
+-- all episode for show id=22 name=Regular Show
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES 
+-- all episode for show id=21 name=The Big Bang Theory
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES 
+	-- all episode for show id=20 Total Drama
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES 
+	
+
+	
+-- all episode for show id=2, Wednesday
+  
+INSERT INTO episode (episode_title, show_id, episode_number, season_number)
+VALUES ('Wednesday\'s Child is Full of Wow', 2, 1, 1), 
+	('Woe Is The Loneliest Number', 2, 2, 1),
+	('Friend or Woe', 2, 3, 1),
+	('Woe What a Night', 2, 4, 1),
+	('You Reap What You Woe', 2, 5, 1),
+	('Quid Prop Woe', 2, 6, 1),
+	('If You Don\'s Woe Me by Now', 2, 7, 1),
+	('A Merder of Woe', 2, 8, 1);
+
   
 INSERT INTO episode (episode_title, show_id, episode_number, season_number)
 VALUES 
